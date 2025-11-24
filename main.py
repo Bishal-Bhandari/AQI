d1ca2de122fcbb28f3a60a1408b7454ea1f593e7
bishal <bishalbhaandari62@gmail.com>
2025-11-24 13:01:53 +0100

    -> Map init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+from flask import Flask, send_from_directory
+import geopandas as gpd
+import json
+import os
+
+app = Flask(__name__)
+
+# Load world GeoJSON
+geo = gpd.read_file(
+    "https://raw.githubusercontent.com/datasets/geo-boundaries-world-110m/master/countries.geojson"
+)
+
+# Filter your country
+COUNTRY = "Nepal"   # Change to any country
+country_geo = geo[geo["ADMIN"] == COUNTRY]
